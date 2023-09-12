560ab446f124370c793eff2e006ee1c21a938f17
Steve Carmichael <steve@techelevator.com>
2023-09-12 12:50:13 -0400

    added final lecture code and day 3 tutorial

---
+++ module-1/02_Variables_and_Datatypes/lecture-final/src/main/java/com/techelevator/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.techelevator;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+
+        String someNumber = "17";
+        System.out.println("Hello World!");
+
+        double sum = 0;
+        double firstValue = 17.35;
+        double secondValue = 18.15;
+
+
+        sum = firstValue + secondValue;
+        System.out.println("The total is: " + sum);
+
+
+    }
+
+
+
+}
