b27605243fadf40ea4e057c8fbf166a8dad79299
Steve Carmichael <steve@techelevator.com>
2023-09-26 13:09:04 -0400

    added final lecture code

---
+++ module-1/12_Polymorphism/lecture-final/src/main/java/com/techelevator/store/Notebook.java
@@ -0,0 +1,17 @@
+package com.techelevator.store;
+
+public class Notebook  {
+
+    private int numberOfPages;
+
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+
+}
