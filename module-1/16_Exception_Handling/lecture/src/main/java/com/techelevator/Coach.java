b78cd1e2e986eed624eb08590cbe2326397b3636
Steve Carmichael <steve@techelevator.com>
2023-10-02 08:50:57 -0400

    added starter code

---
+++ module-1/16_Exception_Handling/lecture/src/main/java/com/techelevator/Coach.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+public class Coach extends Employee{
+
+    public Coach(String firstName, String lastName, int employeeId) {
+        super(firstName, lastName, employeeId);
+    }
+
+    @Override
+    public void printStats() {
+        //don't really print anything here. better to change this later to return the info
+    }
+}
