463dfbfa491b546d07962c8d45dec6f332f09e4d
Nigus Gebremariam <kingsgm19@gmail.com>
2023-10-04 10:10:29 -0400

    Step 11 complete

--- module-1/18_m1_assessment/src/main/java/com/techelevator/MovieRental.java
+++ module-1/18_m1_assessment/src/main/java/com/techelevator/MovieRental.java
@@ -17,6 +17,21 @@ public class MovieRental {
         return "MOVIE: " + title + " - FORMAT:" + format + "PRICE: " + rentalPrice;
 
     }
+    private double calculateRentalPrice() {
+        double basePrice = 0.0;
+        if (format.equals("VHS")) {
+            basePrice = 0.99;
+        } else if (format.equals("DVD")) {
+            basePrice = 1.99;
+        } else if (format.equals("BluRay")) {
+            basePrice = 2.99;
+        }
+
+        if (isPremiumMovie) {
+            basePrice += 1.00;
+        }
+        return basePrice;
+    }
 
 
 
