b43e2de271fbd1465f620359d17ff9ac656fe0f1
Nigus Gebremariam <kingsgm19@gmail.com>
2023-10-04 09:28:22 -0400

    Step 4 and 5 complete

--- module-1/18_m1_assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
+++ module-1/18_m1_assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -3,6 +3,10 @@ package com.techelevator;
 public class Module1CodingAssessment {
 
 	public static void main(String[] args) {
+		MovieRental movie1 = new MovieRental("The Dictator", "DVD", true);
+		MovieRental movie2 = new MovieRental("Titanic", "VHS", true);
+		MovieRental movie3 = new MovieRental("Me and you", "Blue-Ray", false);
+
 		Module1CodingAssessment mca = new Module1CodingAssessment();
 		mca.run();
 	}
--- module-1/18_m1_assessment/src/main/java/com/techelevator/MovieRental.java
+++ module-1/18_m1_assessment/src/main/java/com/techelevator/MovieRental.java
@@ -6,5 +6,9 @@ public class MovieRental {
     boolean isPremiumMovie;
     double rentalPrice;
 
-
+    public MovieRental(String title, String format, boolean isPremiumMovie) {
+        this.title = title;
+        this.format = format;
+        this.isPremiumMovie = isPremiumMovie;
+    }
 }
