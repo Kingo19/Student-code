6f2a922cc31cdc06332eb813f7fe4f0e307ccce6
Steve Carmichael <steve@techelevator.com>
2023-10-04 09:01:14 -0400

    added assessment and starting lecture code

---
+++ module-1/18_Play_Tracker_In_Class_Review/src/main/java/com/techelevator/Roster.java
@@ -0,0 +1,59 @@
+package com.techelevator;
+
+import java.util.*;
+
+public class Roster {
+
+    //INSTANCE VARIABLE
+    private Map<Integer, Employee> employeesMap = new HashMap<>();
+    private ReportWriter writer = new ReportWriter();
+
+    public Roster() {
+        RosterFileReader fileReader = new RosterFileReader();
+        employeesMap = fileReader.loadData();
+        writer.writeToFile("Loaded Player Data From File");
+    }
+
+
+
+    //METHODS
+    public boolean addPlayer (Player newPlayer) {
+        employeesMap.put(newPlayer.getPlayerNumber(), newPlayer);
+        writer.writeToFile("Player added: " +newPlayer.toString());
+        return true;
+    }
+
+    public boolean removePlayer(int employeeId) {
+        employeesMap.remove(employeeId);
+        return true;
+    }
+
+    public boolean removeCoach(int employeeId) {
+        employeesMap.remove(employeeId);
+        return true;
+    }
+
+    public List<Player> retrieveListOfPlayers() {
+
+        List<Player> playersList = new ArrayList<>();
+
+        //LOOP THROUGH THE MAP and CONVERT TO LIST - OPTION 1 (USING KEYSET / SET)
+//        Set<Integer> keys = playerMap.keySet();
+//
+//        for (Integer key : keys) {
+//            Player temp = playerMap.get(key);
+//            playersList.add(temp);
+//        }
+
+        // OPTION 2 USING MAP.ENTRY
+        for(Map.Entry<Integer, Employee> row : employeesMap.entrySet()) {
+            if (row.getValue() instanceof Player) {
+                playersList.add((Player)row.getValue());
+            }
+        }
+
+
+        return playersList;
+    }
+
+}
---
+++ module-1/18_Play_Tracker_In_Class_Review/src/main/java/com/techelevator/Stats.java
@@ -0,0 +1,23 @@
+package com.techelevator;
+
+public class Stats {
+
+    private int atBats;
+    private int hits;
+
+    public int getAtBats() {
+        return atBats;
+    }
+
+    public void setAtBats(int atBats) {
+        this.atBats = atBats;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public void setHits(int hits) {
+        this.hits = hits;
+    }
+}
