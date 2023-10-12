90d654a3078da1f7c25105f05ca8c13c792fc03c
Nigus Gebremariam <kingsgm19@gmail.com>
2023-10-11 22:01:31 -0400

    Module 2 #1

--- module-2/01_Intro_to_Databases/exercise/Exercises/09_high_sales_tax.sql
+++ module-2/01_Intro_to_Databases/exercise/Exercises/09_high_sales_tax.sql
@@ -1,2 +1,3 @@
 -- 9. The name, abbreviation, population, and sales tax of all states and territories with a sales tax greater than 6.6% (9 rows)
-
+SELECT state_abbreviation, state_name, population, sales_tax FROM state
+WHERE sales_tax > 6.6;
