b7480bbe3b0398ee31ca925f1b89c8e1d8c33b67
Nigus Gebremariam <kingsgm19@gmail.com>
2023-10-29 16:32:50 -0400

    Last HW of the week

--- module-2/14_Server_Side_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/14_Server_Side_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -3,6 +3,7 @@ package com.techelevator.auctions.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,43 +16,62 @@ import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.model.Auction;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/auctions")
 public class AuctionController {
 
-    private AuctionDao auctionDao;
+    private final AuctionDao auctionDao;
 
     public AuctionController(AuctionDao auctionDao) {
         this.auctionDao = auctionDao;
     }
 
-    @RequestMapping(path = "", method = RequestMethod.GET)
-    public List<Auction> list(@RequestParam(defaultValue = "") String title_like, @RequestParam(defaultValue = "0") double currentBid_lte) {
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Auction> create(@Valid @RequestBody Auction auction) {
+        Auction createdAuction = auctionDao.createAuction(auction);
 
-        if (!title_like.equals("")) {
-            return auctionDao.getAuctionsByTitle(title_like);
-        }
-        if (currentBid_lte > 0) {
-            return auctionDao.getAuctionsByMaxBid(currentBid_lte);
+        if (createdAuction == null) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Auction could not be created");
         }
 
-        return auctionDao.getAuctions();
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAuction);
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
-    public Auction get(@PathVariable int id) {
+    public ResponseEntity<Auction> get(@PathVariable int id) {
         Auction auction = auctionDao.getAuctionById(id);
+
         if (auction == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Auction Not Found");
         } else {
-            return auctionDao.getAuctionById(id);
+            return ResponseEntity.ok(auction);
         }
     }
 
-    @RequestMapping(path = "", method = RequestMethod.POST)
-    public Auction create(@RequestBody Auction auction) {
-        return auctionDao.createAuction(auction);
+    @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Auction> update(@Valid @RequestBody Auction auction, @PathVariable int id) {
+        Auction existingAuction = auctionDao.getAuctionById(id);
+
+        if (existingAuction == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Auction Not Found");
+        }
+
+        auction.setId(id);
+        Auction updatedAuction = auctionDao.updateAuction(auction);
+        return ResponseEntity.ok(updatedAuction);
     }
 
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        int rowsAffected = auctionDao.deleteAuctionById(id);
 
+        if (rowsAffected == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Auction Not Found");
+        }
+
+        return ResponseEntity.noContent().build();
+    }
 }
