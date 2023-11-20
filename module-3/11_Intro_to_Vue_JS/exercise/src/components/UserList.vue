05a8c127be6fb42d7794e9a60f0c0125fc725762
Nigus Gebremariam <kingsgm19@gmail.com>
2023-11-20 09:15:59 -0500

    Vue Hw

--- module-3/11_Intro_to_Vue_JS/exercise/src/components/UserList.vue
+++ module-3/11_Intro_to_Vue_JS/exercise/src/components/UserList.vue
@@ -1,29 +1,35 @@
 <template>
   <table id="tblUsers">
     <thead>
-    <tr>
+      <tr>
         <th>First Name</th>
         <th>Last Name</th>
         <th>Username</th>
         <th>Email Address</th>
         <th>Status</th>
-    </tr>
+      </tr>
     </thead>
     <tbody>
       <tr>
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
+        <td><input v-model="search.firstName" type="text" id="firstNameFilter" /></td>
+        <td><input v-model="search.lastName" type="text" id="lastNameFilter" /></td>
+        <td><input v-model="search.username" type="text" id="usernameFilter" /></td>
+        <td><input v-model="search.emailAddress" type="text" id="emailFilter" /></td>
         <td>
-          <select id="statusFilter">
+          <select v-model="search.status" id="statusFilter">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Inactive">Inactive</option>
           </select>
         </td>
       </tr>
-      <!-- user listing goes here -->
+      <tr v-for="user in filteredList" :key="user.username" :class="{ 'inactive': user.status === 'Inactive' }">
+        <td>{{ user.firstName }}</td>
+        <td>{{ user.lastName }}</td>
+        <td>{{ user.username }}</td>
+        <td>{{ user.emailAddress }}</td>
+        <td>{{ user.status }}</td>
+      </tr>
     </tbody>
   </table>
 </template>
@@ -39,19 +45,39 @@ export default {
         { firstName: 'Ben', lastName: 'Carter', username: 'bcarter', emailAddress: 'bcarter@gmail.com', status: 'Active' },
         { firstName: 'Katie', lastName: 'Jackson', username: 'kjackson', emailAddress: 'kjackson@yahoo.com', status: 'Active' },
         { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Inactive' }
-      ]
+      ],
+      search: {
+        firstName: '',
+        lastName: '',
+        username: '',
+        emailAddress: '',
+        status: ''
+      }
+    };
+  },
+  computed: {
+    filteredList() {
+      const { firstName, lastName, username, emailAddress, status } = this.search;
+
+      return this.users.filter(user =>
+        user.firstName.toLowerCase().includes(firstName.toLowerCase()) &&
+        user.lastName.toLowerCase().includes(lastName.toLowerCase()) &&
+        user.username.toLowerCase().includes(username.toLowerCase()) &&
+        user.emailAddress.toLowerCase().includes(emailAddress.toLowerCase()) &&
+        (status === '' || user.status.toLowerCase() === status.toLowerCase())
+      );
     }
   }
-}
+};
 </script>
 
 <style scoped>
 table {
   margin-top: 20px;
-  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 th {
-  text-transform: uppercase
+  text-transform: uppercase;
 }
 td {
   padding: 10px;
---
+++ module-3/11_Intro_to_Vue_JS/lecture/src/components/ProductReview.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+    <h2> Product Reviews for: {{ name }}</h2>
+    <p class="description"> desc </p>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            name: 'JavaScript for Dummies',
+            description: 'A super dumb book about JS that is actually really good.'
+        };
+
+    },
+
+}
+</script>
+import ProductReview from './components/ProductReview.vue'
+
+<style scoped>
+div.main {
+    margin: 1rem
+}
+
+</style>
